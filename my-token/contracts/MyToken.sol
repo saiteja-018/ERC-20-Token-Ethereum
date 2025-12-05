4ed1419fa5aaa04a3cea9c3a01fa65fe87afef17
SATYAMSETTI SAI TEJA <saitejasatyamsetti@gmail.com>
2025-12-05 15:37:32 +0530

    Create MyToken.sol

---
+++ my-token/contracts/MyToken.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyToken {
+    // Token metadata
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+
+    // State
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // Constructor - mints all tokens to deployer
+    constructor(uint256 _totalSupply) {
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender] = _totalSupply;
+
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "Cannot approve zero address");
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[_from] >= _value, "Not enough tokens");
+        require(allowance[_from][msg.sender] >= _value, "Insufficient allowance");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function getTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
+
+    function getTokenInfo()
+        public
+        view
+        returns (string memory, string memory, uint8, uint256)
+    {
+        return (name, symbol, decimals, totalSupply);
+    }
+}
